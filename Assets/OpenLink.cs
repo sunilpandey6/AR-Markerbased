fd86cef96cb00a1b49999f4ede04550c74f51c1e
sunilpandey6 <55409916+sunilpandey6@users.noreply.github.com>
2025-09-29 12:05:17 +0200

    updated working

--- Assets/OpenLink.cs
+++ Assets/OpenLink.cs
@@ -6,7 +6,7 @@ using UnityEngine.XR.ARSubsystems;
 
 public class OpenLink : MonoBehaviour
 {
-    private static bool hasOpenedURL = false;  // Static flag to make sure URL is opened only once
+    private static bool hasOpenedURL = false;
 
     [Tooltip("The URL to open when the marker is detected")]
     public string urlToOpen = "https://www.lunduniversity.lu.se/lubas/i-uoh-lu-TAVAR";
@@ -15,22 +15,44 @@ public class OpenLink : MonoBehaviour
 
     void Awake()
     {
-        // Get the ARTrackedImageManager component attached to the XR Origin (or AR Session Origin)
         arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        if (arTrackedImageManager != null)
+        {
+            arTrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (arTrackedImageManager != null)
+        {
+            arTrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+        }
     }
 
     private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs args)
     {
-        // Iterate over all added or updated images
         foreach (var addedImage in args.added)
         {
-            // Check if the image is being tracked (not null and in the "Tracking" state)
             if (!hasOpenedURL && addedImage.trackingState == TrackingState.Tracking)
             {
-                hasOpenedURL = true; // Mark that the URL has been opened
-                Application.OpenURL(urlToOpen);  // Open the URL
+                hasOpenedURL = true;
+                Application.OpenURL(urlToOpen);
             }
         }
 
+        // Optional: Handle updated images
+        foreach (var updatedImage in args.updated)
+        {
+            if (!hasOpenedURL && updatedImage.trackingState == TrackingState.Tracking)
+            {
+                hasOpenedURL = true;
+                Application.OpenURL(urlToOpen);
+            }
+        }
     }
 }
